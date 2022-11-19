0b347ceb1d7b73dff46ac264fef61e4ee0ac83de
Stone <65476500+StoneBelle@users.noreply.github.com>
2022-11-18 22:54:21 -0500

    Update and rename main.py to tk.frame.py

--- tk.frame.py
+++ tk.frame.py
@@ -1,15 +1,15 @@
-## Import Tkinter Library
+# Import Tkinter Library
 from tkinter import *
 
 # Create an instance of Tkinter frame
-win= Tk()
+wn= Tk()
 
 # Set the size of the application window
-win.geometry("700x350")
+wn.geometry("700x350")
 
 # Create a class to define the frame
 class NewFrame(Frame):
-   def __init__(self, win):
+   def __init__(self, wn):
       super().__init__()
       self["height"] = 200
       self["width"] = 200
@@ -18,8 +18,8 @@ class NewFrame(Frame):
       self["bg"] = "#aa11bb"
 
 # Create Frame object
-frame_a= NewFrame(win)
-frame_b= NewFrame(win)
+frame_a= NewFrame(wn)
+frame_b= NewFrame(wn)
 frame_a.grid(row=0, column=0)
 frame_b.grid(row=0, column=1)
 
